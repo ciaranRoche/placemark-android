c05361bc3ff20d9b7d01120af65eb607eadffe75
ciaranRoche <ciaranroche@hotmail.co.uk>
2018-09-14 10:30:26 +0100

    refactor packages, add data class model

--- app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -1,4 +1,4 @@
-package org.wit.placemark
+package org.wit.placemark.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -6,8 +6,11 @@ import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import kotlinx.android.synthetic.main.activity_placemark.*
 import org.jetbrains.anko.toast
+import org.wit.placemark.R
+import org.wit.placemark.models.PlacemarkModel
 
 class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
+  var placemark = PlacemarkModel()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -24,4 +27,5 @@ class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
     }
   }
 }
+
 
