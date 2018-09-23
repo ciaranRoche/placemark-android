ee6455a12bb51049e538942f5669bb3d6acc6f66
ciaranRoche <ciaranroche@hotmail.co.uk>
2018-09-23 15:01:31 +0100

    add cancel button

--- app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -2,10 +2,14 @@ package org.wit.placemark.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import kotlinx.android.synthetic.main.activity_placemark.*
+import kotlinx.android.synthetic.main.activity_placemark_list.*
 import org.jetbrains.anko.toast
+import org.jetbrains.anko.toolbar
 import org.wit.placemark.R
 import org.wit.placemark.main.MainApp
 import org.wit.placemark.models.PlacemarkModel
@@ -19,6 +23,9 @@ class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_placemark)
     app = application as MainApp
+
+    toolbarAdd.title = title
+    setSupportActionBar(toolbarAdd)
 
     btnAdd.setOnClickListener() {
       placemark.title = placemarkTitle.text.toString()
@@ -35,5 +42,17 @@ class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
       }
     }
   }
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.menu_cancel, menu)
+    return super.onCreateOptionsMenu(menu)
+  }
+  override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+    when (item?.itemId) {
+      R.id.item_cancel -> finish()
+    }
+    return super.onOptionsItemSelected(item)
+  }
+
 }
 
