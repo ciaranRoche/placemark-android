f72cfc5885fc4cb6cfbd846d3470b70a0e8e2ed3
ciaranRoche <ciaranroche@hotmail.co.uk>
2018-09-28 10:28:13 +0100

    change button text

--- app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -20,6 +20,8 @@ class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_placemark)
+
+
     toolbarAdd.title = title
     setSupportActionBar(toolbarAdd)
     info("Placemark Activity started..")
@@ -27,9 +29,11 @@ class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
     app = application as MainApp
 
     if (intent.hasExtra("placemark_edit")) {
+      btnAdd.setText(R.string.button_savePlacemark)
       placemark = intent.extras.getParcelable<PlacemarkModel>("placemark_edit")
       placemarkTitle.setText(placemark.title)
       description.setText(placemark.description)
+
     }
 
     btnAdd.setOnClickListener() {
