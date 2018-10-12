99d34f960652607bb68419dd1ae8a1ba68525d36
ciaranRoche <ciaranroche@hotmail.co.uk>
2018-10-12 09:46:36 +0100

    Add map activity and location

---
+++ app/src/main/java/org/wit/placemark/activities/MapsActivity.kt
@@ -0,0 +1,40 @@
+package org.wit.placemark.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import org.wit.placemark.R
+import org.wit.placemark.models.Location
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+  private lateinit var map: GoogleMap
+  var location = Location()
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_maps)
+    location = intent.extras.getParcelable<Location>("location")
+    val mapFragment = supportFragmentManager
+        .findFragmentById(R.id.map) as SupportMapFragment
+    mapFragment.getMapAsync(this)
+  }
+
+  override fun onMapReady(googleMap: GoogleMap) {
+    map = googleMap
+    val loc = LatLng(location.lat, location.lng)
+    val options = MarkerOptions()
+        .title("Placemark")
+        .snippet("GPS : " + loc.toString())
+        .draggable(true)
+        .position(loc)
+    map.addMarker(options)
+    map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, location.zoom))
+  }
+}
